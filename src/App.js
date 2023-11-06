fb9d60b603e019e00c4d38143f83cdde1fcabfd9
Nabeel Arain <137300145+nabeelarain713@users.noreply.github.com>
2023-11-06 14:38:20 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import { HeroSection } from './components/HeroSection';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <HeroSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
